740999853ab1ef57a135659b30f293b22cab38e0
ConanisaCoder <87452767+ConanisaCoder@users.noreply.github.com>
2021-08-18 18:33:14 -0400

    Add files via upload

---
+++ c9-STUD-TEACHER-main/sketch.js
@@ -0,0 +1,20 @@
+
+
+var a = prompt('Enter the first variable: ');
+
+function setup(){
+  var b2 = createButton("click here to Swap");
+b2.mousePress(swap)
+  }
+
+
+function draw()
+{
+
+}
+function swap
+{
+[a,b] = [b,a];
+console.log("Value Of a after Swapping")
+console.log("Value Of b after Swapping"
+}
